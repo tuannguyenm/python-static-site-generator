f34ab78ba2a8c69b95ff2a62314a8814f6fafdae
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-05-13 01:28:15 +0700

    The Content class

---
+++ ssg/content.py
@@ -0,0 +1,51 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = r"^(?:-|+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Load=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        if "type" in self.data:
+            return self.data["type"]  
+        else:
+            return None
+    
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type              
+
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+
+        return str(data)            
