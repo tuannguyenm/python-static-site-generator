cddb2b7384fa7142cdc7ef560ba46d0a42aab64d
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-05-13 11:14:43 +0700

    Parser Subclasses

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -42,7 +42,7 @@ class MarkdownParser(Parser):
         html = markdown(content.body)  
         self.write(path, dest, html)
 
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
@@ -52,4 +52,4 @@ class ReStructuredTextParser(Parser):
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
 
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
